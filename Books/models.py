2f90d0420e1c79f7fd0a8aafeb465bec4ae54a54
S-SMTN <cven28@gmail.com>
2024-10-15 02:10:20 +0300

    added Author model and foreignkey to Book model

--- Books/models.py
+++ Books/models.py
@@ -3,13 +3,21 @@ from django.core.validators import MinValueValidator
 from decimal import Decimal
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
 class Book(models.Model):
     class CoverType(models.TextChoices):
         HARD = 'HARD', 'Hardcover'
         SOFT = 'SOFT', 'Softcover'
 
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(to=Author, on_delete=models.PROTECT)
     cover = models.CharField(max_length=4, choices=CoverType.choices)
     inventory = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     daily_fee = models.DecimalField(
@@ -18,5 +26,5 @@ class Book(models.Model):
         validators=[MinValueValidator(Decimal('0.01'))]
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.title} by {self.author}"
