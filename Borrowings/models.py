394490e660c99147fa13eb52053bd61299a27878
S-SMTN <cven28@gmail.com>
2024-10-15 02:32:34 +0300

    implemented Borrowings model

--- Borrowings/models.py
+++ Borrowings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from datetime import timedelta
+from django.utils import timezone
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey('Book', on_delete=models.PROTECT)
+    user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+
+    @property
+    def expected_return_date(self):
+        return self.borrow_date + timedelta(days=30)
+
+    def __str__(self):
+        return f"Borrowing of {self.book} by {self.user}"
