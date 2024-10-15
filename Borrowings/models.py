23339d92d1369bfe04ca35e5edf0daf413a4d73e
S-SMTN <cven28@gmail.com>
2024-10-15 03:19:04 +0300

    changed expected_return_date from auto calculated to DateTimeField with default auto calculation if None

--- Borrowings/models.py
+++ Borrowings/models.py
@@ -7,14 +7,16 @@ from Books.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateTimeField(auto_now_add=True)
+    borrow_date = models.DateTimeField(auto_now_add=True, editable=False)
+    expected_return_date = models.DateTimeField(null=True, blank=True)
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(to=Book, on_delete=models.PROTECT)
     user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
 
-    @property
-    def expected_return_date(self):
-        return self.borrow_date + timedelta(days=30)
+    def save(self, *args, **kwargs):
+        if self.expected_return_date is None:
+            self.expected_return_date = self.borrow_date + timedelta(days=30)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Borrowing of {self.book} by {self.user}"
