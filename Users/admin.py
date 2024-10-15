9d4078eedc93d42535a9a54c44db29e4e520bd90
S-SMTN <cven28@gmail.com>
2024-10-15 03:38:11 +0300

    registered User model in admin panel

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from Users.models import User
+
+
+@admin.register(User)
+class WorkerAdmin(UserAdmin):
+    search_fields = ["email", "first_name", "last_name"]
+    list_filter = ["email", "first_name", "last_name"]
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "username",
+                        "email"
+                    )
+                },
+            ),
+        )
+    )
