f918b2e9815e8eca5411a37408967cc540b9d048
S-SMTN <cven28@gmail.com>
2024-10-15 03:19:52 +0300

    registered Borrowing model in admin panel

--- Borrowings/admin.py
+++ Borrowings/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from Borrowings.models import Borrowing
+
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = [
+        "borrow_date",
+        "expected_return_date",
+        "actual_return_date",
+        "book",
+        "user"
+    ]
+    list_editable = [
+        "expected_return_date",
+        "actual_return_date",
+        "book",
+        "user"
+    ]
+    list_filter = [
+        "borrow_date",
+        "expected_return_date",
+        "actual_return_date",
+        "book",
+        "user"
+    ]
--- Borrowings/models.py
+++ Borrowings/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from datetime import timedelta
-from django.utils import timezone
 
 from Books.models import Book
 
