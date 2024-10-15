a83da4486434c4d1eed716144572dd3593eac1e5
S-SMTN <cven28@gmail.com>
2024-10-15 03:38:04 +0300

    registered Payment model in admin panel

--- Payments/admin.py
+++ Payments/admin.py
@@ -1,29 +1,33 @@
 from django.contrib import admin
 
-from Books.models import Author, Book
+from Payments.models import Payment
 
 
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
     list_display = [
-        "first_name",
-        "last_name"
-    ]
-
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = [
-        "title",
-        "author",
-        "cover",
-        "inventory",
-        "daily_fee"
+        "status",
+        "payment_type",
+        "borrowing",
+        "session_url",
+        "session_id",
+        "money_to_pay"
     ]
     list_filter = [
-        "title",
-        "author",
-        "cover",
-        "inventory",
-        "daily_fee"
+        "status",
+        "payment_type",
+        "borrowing",
+        "session_url",
+        "session_id",
+        "money_to_pay"
     ]
+    readonly_fields = [field.name for field in Payment._meta.fields]
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
