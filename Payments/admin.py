b39a4ac0957a1b9840f675a806fa12d5a62ee8f3
S-SMTN <cven28@gmail.com>
2024-10-15 03:05:47 +0300

    registered Author and Book models in admin panel

--- Payments/admin.py
+++ Payments/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from Books.models import Author, Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = [
+        "first_name",
+        "last_name"
+    ]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "author",
+        "cover",
+        "inventory",
+        "daily_fee"
+    ]
+    list_filter = [
+        "title",
+        "author",
+        "cover",
+        "inventory",
+        "daily_fee"
+    ]
