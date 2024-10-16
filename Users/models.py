8ed41f2943a8d839e560dec0d96e810ed7929a0f
S-SMTN <cven28@gmail.com>
2024-10-16 16:26:30 +0300

    added library foreignkey to User model

--- Users/models.py
+++ Users/models.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from Public.models import Library
 
 
 class User(AbstractUser):
+    library = models.ForeignKey(
+        Library,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='users'
+    )
 
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name} ({self.email})"
+        return (
+            f"{self.first_name} {self.last_name} ({self.email}), "
+            f"library: {self.library}"
+        )
