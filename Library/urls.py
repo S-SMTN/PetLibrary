27ef093db99382e63c881bf4662561638ef74c92
S-SMTN <cven28@gmail.com>
2024-10-16 15:22:34 +0300

    added admin endpoint to Library urls

--- Library/urls.py
+++ Library/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from django.contrib import admin
+
+from Borrowings.views import factory_borrowing
+from Library.admin import tenant_admin_site
 
 from Library.views import index
 
+from Books.views import factory_book
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", index)
+    path('admin/', tenant_admin_site.urls),
+    path("", index),
+    path("factory_book/", factory_book),
+    path("factory_borrowing", factory_borrowing)
 ]
