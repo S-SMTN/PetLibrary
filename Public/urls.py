10328b204392b0b69db121892f50f8b9f97f6196
S-SMTN <cven28@gmail.com>
2024-10-17 23:17:50 +0300

    implemented library (tenant) endpoints

--- Public/urls.py
+++ Public/urls.py
@@ -1,9 +1,16 @@
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from Public.views import LibraryViewSet
+
+library_router = routers.DefaultRouter()
+library_router.register(prefix="", viewset=LibraryViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/library/", include(library_router.urls)),
     path("api/user/", include("Users.urls", namespace="User")),
     path("__debug__/", include(debug_toolbar.urls))
 ]
