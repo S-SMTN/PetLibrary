5f6f172b73afb3f4a6d0c6e729f797dc303681c8
S-SMTN <cven28@gmail.com>
2025-02-09 03:48:45 +0200

    added spectacular swagger and redoc api documentation

--- PetLibrary/settings.py
+++ PetLibrary/settings.py
@@ -46,6 +46,7 @@ SHARED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular',
     'rest_framework',
     "rest_framework.authtoken",
     'debug_toolbar',
@@ -144,6 +145,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 
